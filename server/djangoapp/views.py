046c73d71b3424fe3c952b1f9431fe71ab269eb1
Przemyslaw Mitura <59741742+1MbeweWhitecrow2@users.noreply.github.com>
2024-11-08 23:49:16 +0000

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -47,7 +47,6 @@ def logout_request(request):
 
 @csrf_exempt
 def registration(request):
-    context = {}
 
     data = json.loads(request.body)
     username = data['userName']
@@ -56,7 +55,6 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    email_exist = False
     try:
         User.objects.get(username=username)
         username_exist = True
@@ -137,10 +135,9 @@ def get_dealer_details(request, dealer_id):
 # def add_review(request):
 
 def add_review(request):
-    if(request.user.is_anonymous == False):
+    if(request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
-            response = post_review(data)
             return JsonResponse({"status":200})
         except:
             return JsonResponse({"status":401,"message":"Error in posting review"})
