f04ad5223241934e0a7bd93285d2ccc3461f461b
Przemyslaw Mitura <59741742+1MbeweWhitecrow2@users.noreply.github.com>
2024-11-08 22:53:35 +0000

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -11,9 +11,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    
     def __str__(self):
         return self.name
-        
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 class CarModel(models.Model):
