d9eca4973fe1833bde9cc51b13e2ee239679ecd7
Przemyslaw Mitura <59741742+1MbeweWhitecrow2@users.noreply.github.com>
2024-11-08 23:50:11 +0000

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS =
-["localhost",
- "https://przemyslawma-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai"]
+["localhost", "https://przemyslawma-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai"]
 
 CSRF_TRUSTED_ORIGINS =
 ["https://przemyslawma-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai"]
